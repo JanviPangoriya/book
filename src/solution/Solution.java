cb311c7b51372eb0ce97ca7ec59850632b2e7c9a
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-09-18 23:10:24 -0700

    IMPORTING SCANNER CLASS MAKING ITS OBJECT NAME OBJ

---
+++ src/solution/Solution.java
@@ -0,0 +1,14 @@
+package solution;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+    }
+}
+
+
+
+}
