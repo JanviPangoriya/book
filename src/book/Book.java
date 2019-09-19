953f3955fdcca7b5d08ce15ca675223da2da90ae
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-09-18 23:08:16 -0700

    make void bookDetails method for printing the desired output.

--- src/book/Book.java
+++ src/book/Book.java
@@ -1,12 +1,23 @@
 package book;
-class Book{
+
+class Book {
     public String name;
     public String author;
     public String an;
-    Book(String name,String author,String an)
-    {
-        this.name=name;
-        this.author=author;
-        this.an=an;
+
+    Book(String name, String author, String an) {
+        this.name = name;
+        this.author = author;
+        this.an = an;
     }
 
+    void bookDetails() {
+        System.out.println("-----------------------------" + "\n"
+                + "Book Name:\t" + name + "\n"
+                + "Author Name:\t" + author + "\n"
+                + "ISBN:\t" + an + "\n"
+                + "-----------------------------");
+    }
+
+}
+}
