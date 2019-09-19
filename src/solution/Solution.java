bb39676a2137ca51b8c83e91e4471827ede7b0ad
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-09-18 23:11:26 -0700

    TAKE VALUE OF VARIABLE A FROM THE USER BY USING OBJECT OF SCANNER CLASS

--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+
 
     }
 }
