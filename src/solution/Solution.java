34860eb8695b7037ad76b2c9bf32ccce2dc2d08e
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-09-18 23:12:44 -0700

    CHECK THE CONDITON NAD THE PRINT THE DESIRED OUTPUT WE USE FOR LOOP FOR HANDLING CONCURRENT OR MANY REQUEST.

--- src/solution/Solution.java
+++ src/solution/Solution.java
@@ -7,7 +7,18 @@ public class Solution {
         Scanner obj = new Scanner(System.in);
         int a = obj.nextInt();
 
-
+        if (a == 0) {
+            System.out.print("N/A");
+        } else {
+            obj.nextLine().trim();
+            for (int i = 1; i <= a; i++) {
+                String name = obj.nextLine().trim();
+                String author = obj.nextLine().trim();
+                String an = obj.nextLine().trim();
+                solution.Solution obj1 = new solution.Solution(name, author, an);
+                obj1.bookDetails();
+            }
+        }
     }
 }
 
