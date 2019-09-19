35e7656e2b85c9a79b7aba83d770a836e3181554
janvipangoriya <janvi.pangoriya_cs18@gla.ac.in>
2019-09-18 23:07:03 -0700

    make constructor of book class and give the value to the variable

--- src/book/Book.java
+++ src/book/Book.java
@@ -3,4 +3,10 @@ class Book{
     public String name;
     public String author;
     public String an;
+    Book(String name,String author,String an)
+    {
+        this.name=name;
+        this.author=author;
+        this.an=an;
+    }
 
